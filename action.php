0fa971d87c74533e140acb6c89ed2725e242459e
lchaconc <luis.chacon.campos@gmail.com>
2017-12-18 10:52:09 -0600

    e mail

---
+++ action.php
@@ -0,0 +1,16 @@
+<?php
+// Capturamos los datos:
+
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$mobile = $_POST['mobile'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$header = 'FROM empresaconstruteccr@gmail.com';
+mail('empresaconstruteccr@gmail.com', $subject,  $message, $mobile, $email );
+echo "Mensaje enviado";
+
+?>
